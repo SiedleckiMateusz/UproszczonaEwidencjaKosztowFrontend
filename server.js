18ea9902872c899ea243c637a637b8203471f98e
Mateusz Siedlecki <siedleckimateus@gmail.com>
2022-07-05 19:05:09 +0200

    configuration for deploy on heroku

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/UproszczonaEwidencjaKosztowFrontend'));
-
+app.use(express.static(__dirname+'/dist/uproszczona-ewidencja-kosztow-frontend'));
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/UproszczonaEwidencjaKosztowFrontend/'}),
-);
+  res.sendFile(path.join(__dirname+'/dist/uproszczona-ewidencja-kosztow-frontend/index.html')));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
